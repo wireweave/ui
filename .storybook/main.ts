0023c6e1368617ee6d00ff57438ccacfed16ca1a
Seungwoo321 <seungwoo321@gmail.com>
2026-07-04 21:48:08 +0900

    style: apply Prettier formatting
    프로젝트 전체에 Prettier 포맷팅을 적용했습니다 (세미콜론 제거, 후행 쉼표 정리).

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,11 +1,8 @@
-import type { StorybookConfig } from '@storybook/react-vite';
+import type { StorybookConfig } from '@storybook/react-vite'
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   framework: {
     name: '@storybook/react-vite',
     options: {},
@@ -14,8 +11,8 @@ const config: StorybookConfig = {
     autodocs: 'tag',
   },
   viteFinal: async (config) => {
-    return config;
+    return config
   },
-};
+}
 
-export default config;
+export default config
